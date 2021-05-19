073daafaa2e8fbde213edc8397f0d3aedea25221
syifa123-ainnur <62318101+syifa123-ainnur@users.noreply.github.com>
2021-05-19 23:27:41 +0700

    update tampilan
    -update footer
    -update loading scene

--- app/Views/index.php
+++ app/Views/index.php
@@ -13,6 +13,9 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="<?= base_url(); ?>/assets/css/style1.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <title>SEMNASTI</title>
         <!-- //favicon// -->
         <link href="<?= base_url(); ?>/assets/img/logosemnasti.png" rel="icon" sizes="16x16 32x32" type="image/png">        
@@ -39,10 +42,10 @@
                     <div class="close">&times;</div>
                     <br>
                     <ul>
-                        <li><a href="index.php">HOME</a></li>
-                        <li><a href="#youwillget">YOU WILL GET</a></li>
-                        <li><a href="#series">OUR SERIES</a></li>
-                        <li><a href="#registration">REGISTATION</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#youwillget">You Wil Get</a></li>
+                        <li><a href="#series">Our Series</a></li>
+                        <li><a href="#registration">Registration</a></li>
                         <!-- <li><a href="#">lorem</a></li>
                         <li><a href="#">lorem</a></li> -->
                     </ul>
@@ -83,7 +86,7 @@
                                             <div class="circle-shape"></div>
                                             <img src="<?= base_url(); ?>/assets/img/direction.png" class="img-fluid" width="" height="" alt="">
                                         </div>                                        
-                                        <h3 class="heading">Strong <BR>Direction</h3>
+                                        <h3 class="heading">Public<BR>Relation</h3>
                                         <p class="hideme">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The
                                         passage is attributed to an unknown typesetter in the 15th century who is thought</p>
                                     </div>
@@ -95,7 +98,7 @@
                                             <img src="<?= base_url(); ?>/assets/img/strategy.png" class="img-fluid" width="" height="" alt="">
                                         </div>                                        
                                         <div class=""></div>
-                                        <h3 class="heading">Deep Strategy</h3>
+                                        <h3 class="heading">Free <br>E-Certificate</h3>
                                         <p class="hideme">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passa
                                         ge is attributed to an unknown typesetter in the 15th century who is thought</p>
                                     </div>
@@ -106,7 +109,7 @@
                                             <div class="circle-shape"></div>
                                             <img src="<?= base_url(); ?>/assets/img/design.png" class="img-fluid" width="" height="" alt="">
                                         </div>                                        
-                                        <h3 class="heading">Rockin' Design</h3>
+                                        <h3 class="heading">Merchandise</h3>
                                         <p class="hideme">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The p
                                         assage is attributed to an unknown typesetter in the 15th century who is thought</p>
                                     </div>
@@ -163,88 +166,107 @@
                     </div>
                 </div>
                 <div class="workclose"><img src="<?= base_url(); ?>/assets/img/wavy2.png" class="img-fluid" width="" height="" alt=""></div>
-                <img src="<?= base_url(); ?>/assets/img/Asset6.png" alt="" class="responsive" width="" height="" id="registration">
-                <div class="limiter" data-aos="zoom-in" data-aos-delay="100" >
-                    <div class="container-login100">
-                        <div class="wrap-login100">
-                            <?php
-                            $inputs = session()->getFlashdata('inputs');
-                            $error = session()->getFlashdata('error');
-                            $errors = session()->getFlashdata('errors');
-                            $success = session()->getFlashdata('success');
-                            if(!empty($errors)){ ?>
-                            <div class="alert alert-danger" role="alert">
-                                <ul>
-                                <?php foreach ($errors as $errors) : ?>
-                                    <li><?= esc($errors) ?></li>
-                                <?php endforeach ?>
-                                </ul>
-                            </div>
-                            <br />
-                            <?php } if(!empty($error)){ ?>
-                            <div class="alert alert-danger" role="alert">
-                                <ul>
-                                    <li><?= esc($error) ?></li>
-                                </ul>
-                            </div>
-                            <br />
-                            <?php } if(!empty($success)){ ?>
-                                <div class="alert alert-success" role="alert">
-                                    <?= esc($success) ?><br />
+                <div class="container">
+                    <img src="<?= base_url(); ?>/assets/img/Asset6.png" alt="" class="responsive" width="" height="" id="registration">
+                    <div class="limiter" data-aos="zoom-in" data-aos-delay="100" >
+                        <div class="container-login100">
+                            <div class="wrap-login100">
+                                <?php
+                                $inputs = session()->getFlashdata('inputs');
+                                $error = session()->getFlashdata('error');
+                                $errors = session()->getFlashdata('errors');
+                                $success = session()->getFlashdata('success');
+                                if(!empty($errors)){ ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <ul>
+                                    <?php foreach ($errors as $errors) : ?>
+                                        <li><?= esc($errors) ?></li>
+                                    <?php endforeach ?>
+                                    </ul>
                                 </div>
                                 <br />
-                            <?php } ?>
-                            <form action="<?= base_url("auth/registration"); ?>" method="post" class="login100-form validate-form">
-                                <?= csrf_field(); ?>
-                                <div class="wrap-input100 validate-input" data-validate = "Masukkan Nama">
-                                    <label>Nama Lengkap : </label>
-                                    <input class="input100" type="text" name="nama" required>
-                                    <span class="focus-input100" ></span>
+                                <?php } if(!empty($error)){ ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <ul>
+                                        <li><?= esc($error) ?></li>
+                                    </ul>
                                 </div>
-
-                                <div class="wrap-input100 validate-input" data-validate="Masukkan Asal Sekolah">
-                                    <label>Asal Instansi : </label>
-                                    <input class="input100" type="text" name="institution" required>
-                                    <span class="focus-input100"></span>
-                                </div>
-
-                                <div class="wrap-input100 validate-input" data-validate="Masukkan Email">
-                                    <label>Email : </label>
-                                    <input class="input100" type="email" name="email" required>	
-                                    <span class="focus-input100"></span>
-                                </div>
-
-                                <div class="wrap-input100 validate-input" data-validate="Masukkan Nomor HP / WhatsApp">
-                                    <label>Nomor HP / WhatsApp : </label>
-                                    <input class="input100" type="number" name="hp" required>
-                                    <span class="focus-input100"></span>
-                                </div>                
-                                <div class="container-login100-form-btn">
-                                    <div class="wrap-login100-form-btn">
-                                        <div class="login100-form-bgbtn"></div>
-                                        <button type="submit" class="login100-form-btn">
-                                        Register
-                                        </button>
+                                <br />
+                                <?php } if(!empty($success)){ ?>
+                                    <div class="alert alert-success" role="alert">
+                                        <?= esc($success) ?><br />
                                     </div>
-                                </div>
-                            </form>
+                                    <br />
+                                <?php } ?>
+                                <form action="<?= base_url("auth/registration"); ?>" method="post" class="login100-form validate-form">
+                                    <?= csrf_field(); ?>
+                                    <div class="wrap-input100 validate-input" data-validate = "Masukkan Nama">
+                                        <label>Nama Lengkap : </label>
+                                        <input class="input100" type="text" name="nama" required>
+                                        <span class="focus-input100" ></span>
+                                    </div>
+
+                                    <div class="wrap-input100 validate-input" data-validate="Masukkan Asal Sekolah">
+                                        <label>Asal Instansi : </label>
+                                        <input class="input100" type="text" name="institution" required>
+                                        <span class="focus-input100"></span>
+                                    </div>
+
+                                    <div class="wrap-input100 validate-input" data-validate="Masukkan Email">
+                                        <label>Email : </label>
+                                        <input class="input100" type="email" name="email" required>	
+                                        <span class="focus-input100"></span>
+                                    </div>
+
+                                    <div class="wrap-input100 validate-input" data-validate="Masukkan Nomor HP / WhatsApp">
+                                        <label>Nomor HP / WhatsApp : </label>
+                                        <input class="input100" type="number" name="hp" required>
+                                        <span class="focus-input100"></span>
+                                    </div>                
+                                    <div class="container-login100-form-btn">
+                                        <div class="wrap-login100-form-btn">
+                                            <div class="login100-form-bgbtn"></div>
+                                            <button type="submit" class="login100-form-btn">
+                                            Register
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="workclose"><img src="<?= base_url(); ?>/assets/img/wavy.png" class="img-fluid" width="" height="" alt=""></div>
                 <div class="workclose"><img src="<?= base_url(); ?>/assets/img/hiring.png" class="img-fluid" width="" height="" alt=""></div>
             </section>
-            <footer>
-                <div class="footer-content">
-                    <h3>SEMNASTI</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo iste corrupti doloribus odio sed!</p>
-                    <ul class="socials">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
-                    </ul>
+            <footer class="footer">
+                <div class="section__heading">
+                    <h2 class="section__title"><span class="heading-stroke" data-title="SEMNASTI">SEMNASTI</span></h2>
+                </div>
+                <div class="isi">
+                    <div class="row">
+                        <div class="footer-col">
+                            <h4>Contact Person</h4>
+                            <ul>
+                                <li><a href="https://api.whatsapp.com/send?phone=6285329683273"><i class="fa fa-whatsapp" style="font-size: 25px; color: white; padding-right:10px;"></i>0822-4169-8249 (Ekki)</a></li>
+                                <li><a href="https://api.whatsapp.com/send?phone=6285158799141"><i class="fa fa-whatsapp" style="font-size: 25px; color: white; padding-right:10px;"></i>0853-2662-9159 (Akbar)</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-col">
+                            <h4>Sponshorship</h4>
+                            <ul>
+                                <li><a href="https://api.whatsapp.com/send?phone=6285329683273"><i class="fa fa-whatsapp" style="font-size: 25px; color: white; padding-right:10px;"></i>085329683273 (Raka)</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer-col">
+                            <h4>follow us</h4>
+                            <div class="social-links">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="footer-bottom">
                     <p>copyright &copy;2021 designed by <span>syifa</span></p>
@@ -252,7 +274,7 @@
             </footer>
         </main>
         <div id="preloader">
-            <img class="image animate__animated animate__fadeOut" src="<?= base_url(); ?>/assets/img/logosemnasti.png">
+            <img class="animate-flicker" src="<?= base_url(); ?>/assets/img/logosemnasti.png">
             <!--<img class="image2" src="<?= base_url(); ?>/assets/img/tulisant.png">-->
         </div>
         <!-- Optional JavaScript -->
